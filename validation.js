ee09c4feff1a65d8e3c1b61e04c3dc84c251eb37
“Mariana <zaira.vl21@gmail.com>
2023-05-26 08:54:28 -0600

    Initializes code refactor of mdlinks file

--- validation.js
+++ validation.js
@@ -2,11 +2,11 @@ const path = require('node:path')
 const fs = require('fs')
 // Function that validates if the route exists and whether or not it is absolute
 const validateRoute = (route) => {
-  const existsRoute = fs.existsSync(route)
-  const newRoute = path.isAbsolute(route) ? route : path.resolve(route)
-  return {
-    exists: existsRoute,
-    route: newRoute
+  const existsRoute = fs.existsSync(route) // Checks that the route does exist
+  const newRoute = path.isAbsolute(route) ? route : path.resolve(route) // If it's absolute, returns it as is, if not, turns it into absolute
+  return { // returns an object
+    exists: existsRoute, // true or false depending on its existence
+    route: newRoute // returns the absolute route
   }
 }
 
